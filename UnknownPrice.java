552d462d201a31f4e3459f60c8de61c467ffe52b
CaiHughes <66554578+CaiHughes@users.noreply.github.com>
2026-03-27 18:10:47 +0000

    Delete UnknownPrice.java

--- UnknownPrice.java
+++
@@ -1,9 +0,0 @@
-
-public final class UnknownPrice implements Price{
-
-    @Override
-    @NotNull
-    public FinancialValue calculate(@NotNull final Quantity quantity){
-        return new UnknownFinancialValue(quantity);
-    }
-}
